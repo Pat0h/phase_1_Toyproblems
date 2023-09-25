83259ce071ea55b62aaf42df234f743535e5956d
Patrick Ngatia <patrickngatia14@gmail.com>
2023-09-25 23:49:09 +0300

    prompt attempt

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 
-function totalGrade(){
+
+function totalGrade(mark){
+  function inputGrade() {
+    const mark = parseFloat(prompt("Enter the student's mark (between 0 and 100):"));
+  
+  }
   if (mark < 0 || mark >100){
-    console.log('Please enter valid marks!')
-  }else if (mark > 79){
+    return 'Please enter valid marks!'
+  }else if (mark > 79 && mark <=100){
   return 'A';
 }
-else if (mark >= 60){
+else if (mark >= 60 && mark <= 79){
 return 'B';
-}else if (mark >= 49){
+}else if (mark >= 49 && mark <= 59){
   return ' C';
-}else if (mark >= 40){
+}else if (mark >= 40 && mark <= 50){
   return 'D';
 } else {
   return 'E'
 }
-}
-
-function getUserInputAndCalculateGrade() {
-  const mark = parseFloat(prompt("Enter the student's mark (between 0 and 100):"));
 
 }
+
+
 totalGrade();
